2fcbc96298047875829df26a6503c62f63ba2c1b
Luciano Mendonca <luciano.s.mend@gmail.com>
2025-02-02 16:16:30 -0300

    first commit

---
+++ src/main/java/br/luciano/bytebank/BytebankBatchApplication.java
@@ -0,0 +1,13 @@
+package br.luciano.bytebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytebankBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytebankBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/luciano/bytebank/BytebankBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.luciano.bytebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytebankBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
